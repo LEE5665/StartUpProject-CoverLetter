ee6c6c69c7aa2210c4dc5a43a5e3acec2d925e91
LEE <yttr309@naver.com>
2025-10-23 16:40:48 +0900

    이미지 문제 수정

--- Project/src/pages/Projects.jsx
+++ Project/src/pages/Projects.jsx
@@ -1,9 +1,9 @@
 import "./Projects.css";
-import project1 from "/project/project1.png";
-import project2 from "/project/project2.png";
-import project3 from "/project/project3.png";
-import project4 from "/project/project4.png";
-import project5 from "/project/project5.png";
+import project1 from "/project1.png";
+import project2 from "/project2.png";
+import project3 from "/project3.png";
+import project4 from "/project4.png";
+import project5 from "/project5.png";
 
 export default function Projects() {
   return (
